9102ec276db9c999175044c6b20ab5fd3aa7f8f5
Raphi W <ditshej@gmail.com>
2021-04-10 19:29:48 +0200

    EP7

---
+++ resources/views/post.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post</title>
+</head>
+<body>
+
+<article>
+    <h1><a href="/post">Hello World</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad amet assumenda beatae doloribus dolorum, illum quasi
+        quidem quisquam quo sequi. Accusamus dolores earum eum iste nihil possimus, quia reiciendis saepe. Architecto
+        aut cum excepturi facere impedit, iusto maiores nam nihil obcaecati optio quos repudiandae sed, tempora. Ab,
+        consequatur magni odio provident soluta ut veritatis voluptate? Accusamus aliquam beatae enim necessitatibus
+        optio quaerat quidem recusandae sapiente tenetur veniam. Aliquam at beatae, consequatur debitis eos, fuga
+        impedit, libero magni maiores minus modi mollitia natus nostrum pariatur placeat quam qui quod recusandae
+        repellendus reprehenderit rerum sint sunt tempora vel voluptate? Atque blanditiis consectetur eligendi impedit
+        incidunt maiores molestias odit officia sapiente voluptatum! Accusantium architecto autem corporis earum eos
+        incidunt quibusdam repudiandae sapiente vero voluptate. Adipisci aspernatur corporis dolor enim eveniet fuga
+        nihil, numquam obcaecati perspiciatis porro quas qui quos rerum saepe sapiente sed sit suscipit tempora
+        temporibus veniam vero voluptatem voluptatum. Commodi dicta eum explicabo fugit hic provident quaerat? Dolorem,
+        ducimus earum eos inventore ipsam quae qui repudiandae, suscipit tempora tenetur totam veritatis. Accusantium
+        dignissimos dolores eius eligendi facilis ipsa iure maxime mollitia optio quas, quia quidem quis, temporibus
+        veniam voluptatibus. Accusamus alias asperiores aspernatur corporis illo iusto perspiciatis porro sequi tempore
+        voluptate?</p>
+</article>
+<a href="/">Go Back</a>
+</body>
+</html>
