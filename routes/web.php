6f003711732b1078561eef59db50e1f6e6411af7
Raphi W <ditshej@gmail.com>
2021-04-10 19:48:16 +0200

    EP9

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,11 @@ Route::get('posts/{post}', function ($slug) {
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
     if (!file_exists($path)) {
-
-        // abort('404');
+        abort('404');
     }
 
     $post = file_get_contents($path);
     return view('post', [
         'post' => $post
     ]);
-});
+})->where('post', '[A-z_\-]+');
