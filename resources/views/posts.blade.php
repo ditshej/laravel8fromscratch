0f35dca6fd8927857bd3f1d08785ea623dabdbce
Raphi W <ditshej@gmail.com>
2021-04-11 10:19:56 +0200

    EP14

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,17 +9,17 @@
     <link rel="stylesheet" href="/app.css"/>
 </head>
 <body>
-<?php foreach ($posts as $post) : ?>
-<article>
-    <h1>
-        <a href="/posts/<?= $post->slug; ?>">
-            <?= $post->title; ?>
-        </a>
-    </h1>
-    <div>
-        <?= $post->excerpt; ?>
-    </div>
-</article>
-<?php endforeach; ?>
+@foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{ $post->slug }}">
+                {{ $post->title }}
+            </a>
+        </h1>
+        <div>
+            {{ $post->excerpt }}
+        </div>
+    </article>
+@endforeach
 </body>
 </html>
