5d58a642e67f9922777bd648665d0d99b1716a75
Raphi W <ditshej@gmail.com>
2021-04-11 10:28:39 +0200

    EP15.1

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,11 @@
-<!doctype html>
-<html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Post</title>
-    <link rel="stylesheet" href="/app.css"/>
-</head>
-<body>
-<article>
-    <h1>{{ $post->title }}</h1>
-    <div>
-        {!! $post->body !!}
-    </div>
-</article>
-<a href="/posts">Go Back</a>
-</body>
-</html>
+@extends('layout')
+
+@section('content')
+    <article>
+        <h1>{{ $post->title }}</h1>
+        <div>
+            {!! $post->body !!}
+        </div>
+    </article>
+    <a href="/posts">Go Back</a>
+@endsection
