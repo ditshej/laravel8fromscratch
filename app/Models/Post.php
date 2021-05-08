5d8b88a83421721bc281950100a6eba9159675df
Raphi W <ditshej@gmail.com>
2021-05-08 14:19:11 +0200

    EP23.4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,9 +10,4 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'excerpt', 'body'];
-
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
 }
