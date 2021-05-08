af6823a97d574a87b333d13d0b603267d27735a0
Raphi W <ditshej@gmail.com>
2021-05-08 15:15:01 +0200

    EP25

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,9 @@ Route::get('posts/{post:slug}', function (Post $post) {
         'post' => $post
     ]);
 });
+
+Route::get('categories/{category:slug}', function (\App\Models\Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
+});
