495e6af64c10b68d25edb4369000606d2969fffc
Raphi W <ditshej@gmail.com>
2021-05-10 08:56:31 +0200

    EP26

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('posts', function () {
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
