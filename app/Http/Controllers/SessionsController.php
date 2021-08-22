cbb859a2b8f8bb85874a9422837c6b2e260b3fb7
Raphi W <ditshej@gmail.com>
2021-08-22 17:53:48 +0200

    EP50

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,8 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionsController extends Controller
 {
+
+    public function create()
+    {
+        return view('session.create');
+    }
+
+    public function store()
+    {
+        $attribues = request()?->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attribues)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
