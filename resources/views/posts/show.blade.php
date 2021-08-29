ad6de862a5be3b60afa553e901d9c52c6ad2d7f3
Raphi W <ditshej@gmail.com>
2021-08-29 09:40:47 +0200

    EP55 - Design the Comment Form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,18 +50,20 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                <x-panel>
-                    <form method="POST" action="">
-                        @csrf
 
-                        <header class="flex items-center">
-                            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40"
-                                 class="rounded-full">
+                @auth
+                    <x-panel>
+                        <form method="POST" action="/posts/{{ $post->slug }}/comments">
+                            @csrf
 
-                            <h2 class="ml-4">Want to participate?</h2>
-                        </header>
+                            <header class="flex items-center">
+                                <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="" width="40" height="40"
+                                     class="rounded-full">
 
-                        <div class="mt-6">
+                                <h2 class="ml-4">Want to participate?</h2>
+                            </header>
+
+                            <div class="mt-6">
                         <textarea
                             class="w-full text-sm focus:outline-none focus:ring"
                             cols="30"
@@ -69,18 +71,23 @@
                             name="body"
                             placeholder="Quick, think of something to say"
                             rows="5"></textarea>
-                        </div>
+                            </div>
 
-                        <div class="flex justify-end mt-6 pt-6 border-t boder-gray-200">
-                            <button
-                                class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
-                                type="submit"
-                            >
-                                Post
-                            </button>
-                        </div>
-                    </form>
-                </x-panel>
+                            <div class="flex justify-end mt-6 pt-6 border-t boder-gray-200">
+                                <button
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600"
+                                    type="submit"
+                                >
+                                    Post
+                                </button>
+                            </div>
+                        </form>
+                    </x-panel>
+                @else
+                    <p class="font-semibold">
+                        <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">log in</a> to leave a comment.
+                    </p>
+                @endauth
 
                 @foreach($post->comments as $comment)
                     <x-post-comment :comment="$comment"/>
